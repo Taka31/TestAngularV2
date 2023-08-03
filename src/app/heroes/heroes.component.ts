3880f5057a7627bd7770d8598ad9fd68e3f7704f
Cyril Menard <cyrilus@MacBook-Pro-de-Cyril.local>
2023-08-03 19:48:36 +0200

    Heroes Tower tuto Part 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,11 +10,25 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent {
 
-  heroes = HEROES;
+  heroes : Hero[] = [];
   selectedHero?:Hero;
+
+  constructor(private heroService : HeroService, private messageService : MessageService){
+
+  }
+
+  ngOnInit() :void {
+    this.getHeroes();
+  }
 
   onSelect(hero:Hero){
     this.selectedHero=hero;
+    this.messageService.add(`HeroesComponent : Selected Hero id=${hero.id}`);
+
+  }
+
+  getHeroes() : void {
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes = heroes);
   }
 
 }
